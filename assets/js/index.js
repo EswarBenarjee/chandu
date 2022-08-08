718e36f49125fc05959f5ca9e85262765691e5d2
CodeBreaker231 <eswar.bane2002.India@gmail.com>
2022-08-08 12:28:29 +0530

    Updated filters

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,6 +15,19 @@ for (let index = 0; index < filters.length; index++) {
         video.currentTime = 0;
         video.play();
     });
+    filter.addEventListener('click', () => {
+        let filterid = filter.id;
+        let number = filterid[filterid.length-1];
+
+        let img = document.getElementById('filterImg'+number);
+        img.style.display = 'none';
+
+        let video = document.getElementById('filterVideo'+number);
+        video.style.display = 'block';
+
+        video.currentTime = 0;
+        video.play();
+    });
     filter.addEventListener('mouseleave', () => {
         let filterid = filter.id;
         let number = filterid[filterid.length-1];
